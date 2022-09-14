f878615dc234c1af912f5f3dad8d30b2b4f0065b
LRapier <98057284+LRapier@users.noreply.github.com>
2022-09-14 00:13:41 -0700

    Plague-V1

---
+++ Assets/GameManager.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    [Header("Stats")]
+    public bool gameEnded = false;
+    public float timeToDie;
+    public float invincibleDuration;
+    private float hatPickupTime;
+
+    [Header("Players")]
+    public string playerPrefabLocation;
+    public Transform[] spawnPoints;
+    public PlayerController[] players;
+    public int playerWithHat;
+    private int playersInGame;
+
+    [HideInInspector]
+    public int playersNum;
+    public int playersDead;
+
+    public static GameManager instance;
+
+    void Awake()
+    {
+        // instance
+        instance = this;
+    }
+
+    void Start()
+    {
+        players = new PlayerController[PhotonNetwork.PlayerList.Length];
+        photonView.RPC("ImInGame", RpcTarget.AllBuffered);
+    }
+
+    [PunRPC]
+    void ImInGame()
+    {
+        playersInGame++;
+        playersNum++;
+        if (playersInGame == PhotonNetwork.PlayerList.Length)
+            SpawnPlayer();
+    }
+
+    void SpawnPlayer()
+    {
+        GameObject playerObj = PhotonNetwork.Instantiate(playerPrefabLocation, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.Euler(0, 45, 0));
+        PlayerController playerScript = playerObj.GetComponent<PlayerController>();
+        playerScript.photonView.RPC("Initialize", RpcTarget.All, PhotonNetwork.LocalPlayer);
+    }
+
+    public PlayerController GetPlayer (int playerId)
+    {
+        return players.First(x => x.id == playerId);
+    }
+
+    public PlayerController GetPlayer(GameObject playerObj)
+    {
+        return players.First(x => x.gameObject == playerObj);
+    }
+
+    [PunRPC]
+    public void GiveHat (int playerId, bool initialGive)
+    {
+        if (!initialGive)
+            GetPlayer(playerWithHat).SetHat(false);
+
+        playerWithHat = playerId;
+        GetPlayer(playerId).SetHat(true);
+        hatPickupTime = Time.time;
+    }
+
+    [PunRPC]
+    public void GiveAwayPoison(int victimId)
+    {
+        GetPlayer(playerWithHat).SetHat(false);
+        playerWithHat = victimId;
+        GetPlayer(victimId).SetHat(true);
+        hatPickupTime = Time.time;
+    }
+
+    public bool CanGetHat()
+    {
+        if (Time.time > hatPickupTime + invincibleDuration)
+            return true;
+        else
+            return false;
+    }
+
+    [PunRPC]
+    void WinGame(int playerId)
+    {
+
+        Debug.Log("Game Over");
+        gameEnded = true;
+        PlayerController player = GetPlayer(playerId);
+        GameUI.instance.SetWinText(player.photonPlayer.NickName);
+
+        Invoke("GoBackToMenu", 3.0f);
+    }
+
+    void GoBackToMenu()
+    {
+        PhotonNetwork.LeaveRoom();
+        NetworkManager.instance.ChangeScene("Main Menu");
+    }
+
+    [PunRPC]
+    void KillPlayer(int playerId)
+    {
+        PlayerController player = GetPlayer(playerId);
+        int successorId = Random.Range(1, 4);
+        while(GetPlayer(successorId).player.activeSelf == false)
+            successorId = Random.Range(1, 4);
+        GiveHat(successorId, false);
+        ++playersDead;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,154 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
+{
+    [HideInInspector]
+    public int id;
+    public bool isPoisoned;
+
+    [Header("Info")]
+    public float moveSpeed;
+    public float jumpForce;
+    public GameObject hatObject;
+    
+    [Header("Materials")]
+    public Material player1;
+    public Material player2;
+    public Material player3;
+    public Material player4;
+
+    [HideInInspector]
+    public float curHatTime;
+
+    [Header("Components")]
+    public Rigidbody rig;
+    public GameObject player;
+    public Player photonPlayer;
+
+    [PunRPC]
+    public void Initialize(Player player)
+    {
+        photonPlayer = player;
+        id = player.ActorNumber;
+        GameManager.instance.players[id - 1] = this;
+
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+        switch (id)
+        {
+            case 1:
+                GameManager.instance.GiveHat(id, true);
+                renderer.material = player1;
+                break;
+            case 2:
+                renderer.material = player2;
+                break;
+            case 3:
+                renderer.material = player3;
+                break;
+            case 4:
+                renderer.material = player4;
+                break;
+        }
+
+        if (!photonView.IsMine)
+            rig.isKinematic = true;
+
+        curHatTime = GameManager.instance.timeToDie;
+    }
+
+    void Update()
+    {
+        if(PhotonNetwork.IsMasterClient)
+        {
+            if((curHatTime <= 0) && !GameManager.instance.gameEnded)
+            {
+                GameManager.instance.photonView.RPC("KillPlayer", RpcTarget.All, id);
+                KillPlayer();
+            }
+        }
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            if (GameManager.instance.playersDead == GameManager.instance.playersNum - 1)
+            {
+                GameManager.instance.photonView.RPC("WinGame", RpcTarget.All, id);
+            }
+        }
+
+        if (photonView.IsMine)
+        {
+            Move();
+            if (Input.GetKeyDown(KeyCode.Space))
+                TryJump();
+            if (hatObject.activeInHierarchy)
+                curHatTime -= Time.deltaTime;
+        }
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal") * moveSpeed;
+        float z = Input.GetAxis("Vertical") * moveSpeed;
+        rig.velocity = new Vector3(x, rig.velocity.y, z);
+    }
+
+    void TryJump()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        if (Physics.Raycast(ray, 0.7f))
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    public void SetHat (bool hasHat)
+    {
+        hatObject.SetActive(hasHat);
+        isPoisoned = true;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!photonView.IsMine)
+            return;
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("player touch");
+            if ((GameManager.instance.GetPlayer(collision.gameObject).id != GameManager.instance.playerWithHat) && isPoisoned == true)
+            {
+                Debug.Log("you are poison and they are not");
+                if (GameManager.instance.CanGetHat())
+                {
+                    Debug.Log("Give away poison");
+                    GameManager.instance.photonView.RPC("GiveAwayPoison", RpcTarget.All, GameManager.instance.GetPlayer(collision.gameObject).id);
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider death)
+    {
+        if (death.gameObject.CompareTag("Death"))
+            Respawn();
+    }
+
+    public void OnPhotonSerializeView (PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+            stream.SendNext(curHatTime);
+        else if(stream.IsReading)
+            curHatTime = (float)stream.ReceiveNext();
+    }
+
+    public void KillPlayer()
+    {
+        player.SetActive(false);
+    }
+
+    public void Respawn()
+    {
+        player.transform.position = new Vector3(0, 8, 0);
+    }
+}
